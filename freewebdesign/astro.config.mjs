1bdd55a6f203ad3084db4e40d1cc310b05631163
EseToni <falsigfx@gmail.com>
2024-03-04 21:43:02 +0100

    [MODIFIED] @astro.config.mjs ##messagecommit

--- freewebdesign/astro.config.mjs
+++ freewebdesign/astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: netlify()
 });
